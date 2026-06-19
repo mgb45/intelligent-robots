f182e54ff54359bcba8bf704d1e28a7fd2536157
Michael <you@example.com>
2026-06-19 16:38:52 +1000

    Tweaked config for deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,16 +16,13 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'MonashRobotics', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  // --- CHANGED FOR GITHUB PAGES DEPLOYMENT ---
+  url: 'https://michaelburke.co.za',
+  baseUrl: '/intelligent-robots/',
+  organizationName: 'mgb45', 
+  projectName: 'intelligent-robots', 
+  trailingSlash: false, 
+  // --------------------------------------------
 
   onBrokenLinks: 'throw',
 
@@ -45,12 +42,10 @@ const config: Config = {
           routeBasePath: '/',
           path: 'docs/robotics-unit',
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
           remarkPlugins: [remarkMath],
           rehypePlugins: [[rehypeKatex, {strict: false}]],
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mgb45/intelligent-robots/tree/main/docs/robotics-unit/',
         },
         blog: false,
         pages: false,
@@ -72,7 +67,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
